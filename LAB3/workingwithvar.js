54a88442af57753c6f9a2150c365cc6d288a740f
alisherakim <78424859+alisherakim@users.noreply.github.com>
2022-02-14 23:07:43 +0600

    Add files via upload

---
+++ LAB3/workingwithvar.js
@@ -0,0 +1,10 @@
+let admin, name; 
+name = "John";
+admin = name;
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+alert( admin );
+const birthday = '18.04.1982';
+const age = someCode(birthday);
+const BIRTHDAY = '18.04.1982'; 
+const AGE = someCode(BIRTHDAY);
